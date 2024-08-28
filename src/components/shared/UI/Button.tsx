c2bfd1ef34a609b971347247ff897e017d0abaa1
dangquang <dangquang@duck.com>
2024-08-28 22:36:42 +0700

    comment btn

--- src/components/shared/UI/Button.tsx
+++ src/components/shared/UI/Button.tsx
@@ -1,11 +1,17 @@
-import { Button } from 'react-native-paper';
+import { Button, IconButton } from 'react-native-paper';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  mode?: 'sign-in' | 'continue' | 'skip' | 'arrow';
+  isButton?: boolean;
+  size?: 'small' | 'medium' | 'large';
+  rounded?: boolean;
   onPress?: () => void;
 }
 
-export default function MyButton({ children, onPress, ...props }: ButtonProps) {
-  return <Button mode='contained'>{children}</Button>;
-}
+// export default function CustomButton({ children, isButton, size, ...rest }: ButtonProps) {
+//   return isButton ? (
+//     <IconButton icon={children} {...rest} />
+//   ) : (
+//     <Button {...rest}>{children}</Button>
+//   );
+// }
