293301e8c5bf15a71bc46f938f652fe4a655d2b7
Quindarts <quang82thcspb@gmail.com>
2024-10-08 00:36:55 +0700

    fix: styling axios-config

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,10 +1,8 @@
 import axiosConfig from './axiosConfig';
-import axios, { AxiosResponse } from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { IAuth } from '@/types/auth.type';
 
 const login: any = async ({ email, password }: IAuth) => {
-  console.log(email, password);
   return await axiosConfig.post('/auth/login', {
     email,
     password,
